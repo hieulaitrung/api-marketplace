5604262066ff02a2a94475d9203ea3ee52967e64
Hieu Lai Trung <v.hieult11@vinid.net>
2020-06-08 09:17:57 +0700

    Add redis test container

--- src/main/java/com/example/demo/config/EsConfig.java
+++ src/main/java/com/example/demo/config/EsConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
